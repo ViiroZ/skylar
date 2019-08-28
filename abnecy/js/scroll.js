e65cf86eb3b7cbca05cd4f5caa34317adbc6ac8a
ViiroZ <47350351+ViiroZ@users.noreply.github.com>
2019-08-28 18:20:58 +0300

    upd

--- abnecy/js/scroll.js
+++ abnecy/js/scroll.js
@@ -1,3 +1,19 @@
 $(".anchor a").mPageScroll2id({
    offset : 60
 });
+
+$(".anchor a").mPageScroll2id({
+   offset : 60
+});
+
+
+
+// $('.anchor-1').click(function() {
+//   $('.on').prop('checked', false);
+//   $('.off').prop('checked', true);
+// });
+
+// $('.anchor-2').click(function() {
+//   $('.on').prop('checked', true);
+//   $('.off').prop('checked', false);
+// });
